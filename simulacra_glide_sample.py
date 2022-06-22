46fa83d91ae0c393ac648a490145ea8931c63cf7
spuun <69007078+spuuntries@users.noreply.github.com>
2022-06-22 16:08:20 +0700

    Update simulacra_glide_sample.py
    reverting?

--- simulacra_glide_sample.py
+++ simulacra_glide_sample.py
@@ -50,7 +50,7 @@ def spherical_dist_loss(x, y):
 
 def load_model_from_config(config, ckpt, verbose=False):
     print(f"Loading model from {ckpt}")
-    pl_sd = torch.load(ckpt, map_location="cuda")
+    pl_sd = torch.load(ckpt, map_location="cpu")
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
     m, u = model.load_state_dict(sd, strict=False)
@@ -62,7 +62,7 @@ def load_model_from_config(config, ckpt, verbose=False):
         print(u)
 
     model.cuda()
-    model.half()
+    model.eval()
     return model
 
 
